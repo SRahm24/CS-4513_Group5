414c97cb5d7f11d384a5336e924058a142c2c2c9
[carlsullivan] <[ces8059@utulsa.edu]>
2024-02-28 21:18:28 -0600

    Organized the UI components into a folder structure. Also added the firebase and queries files to the database folder. Implemented basic queries for the database.

---
+++ server-buddy/src/database/firebase.tsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAS5c1hC7oDqDnutJnW4CJoHy1If5fmXEE",
+  authDomain: "serverbuddy-3daa7.firebaseapp.com",
+  projectId: "serverbuddy-3daa7",
+  storageBucket: "serverbuddy-3daa7.appspot.com",
+  messagingSenderId: "794480954381",
+  appId: "1:794480954381:web:62914f7bf724e184ced944",
+  measurementId: "G-ZH0QMHLEVR"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
