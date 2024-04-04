80471ed78c148497c51b5d5a91810757e475fb05
[carlsullivan] <[ces8059@utulsa.edu]>
2024-04-03 21:49:03 -0500

    Refactor database setters and sample orders

--- server-buddy/src/database/setters/setters.tsx
+++ server-buddy/src/database/setters/setters.tsx
@@ -10,11 +10,12 @@ export class setters {
     /*
     pushOrder takes an order object and pushes it to the database
     orderDateTime is a timestamp from the firebase server
+    document reference is set by the firebase server
     Order object is defined in objects/order.tsx
      */
     static async pushOrder(order: Order) {
         const orderRef = collection(db, "Orders");
-        const orderDoc = doc(orderRef, order.getOrderId());
+        const orderDoc = doc(orderRef);
         await setDoc(orderDoc, {
             orderId: order.getOrderId(),
             ticketId: order.getTicketId(),
@@ -30,11 +31,12 @@ export class setters {
     /*
     pushTicket takes a ticket object and pushes it to the database
     ticketTime is a timestamp from the firebase server
+    document reference is set by the firebase server
     Ticket object is defined in objects/ticket.tsx
     */
     static async pushTicket(ticket: Ticket) {
         const ticketRef = collection(db, "Tickets");
-        const ticketDoc = doc(ticketRef, ticket.getTicketId());
+        const ticketDoc = doc(ticketRef);
         await setDoc(ticketDoc, {
             ticketId: ticket.getTicketId(),
             tableId: ticket.getTableId(),
@@ -47,12 +49,12 @@ export class setters {
 
     /* 
     pushRestaurant takes a restaurant object and pushes it to the database
+    document reference is set by the firebase server
     Restaurant object is defined in objects/restaurant.tsx
     */
-
     static async pushRestaurant(restaurant: Restaurant) {
         const restaurantRef = collection(db, "Restaurants");
-        const restaurantDoc = doc(restaurantRef, restaurant.getRestaurantId());
+        const restaurantDoc = doc(restaurantRef);
         await setDoc(restaurantDoc, {
             restaurantId: restaurant.getRestaurantId(),
             restaurantName: restaurant.getRestaurantName(),
@@ -68,11 +70,12 @@ export class setters {
 
     /*
     pushItem takes an item object and pushes it to the database
+    document reference is set by the firebase server
     Item object is defined in objects/menuItem.tsx
     */
     static async pushItem(item: Item) {
         const itemRef = collection(db, "Items");
-        const itemDoc = doc(itemRef, item.getItemId());
+        const itemDoc = doc(itemRef);
         await setDoc(itemDoc, {
             itemName: item.getItemName(),
             itemId: item.getItemId(),
