c4face36dd4275ba30914806273cfdff6c947407
[carlsullivan] <[ces8059@utulsa.edu]>
2024-04-17 14:18:06 -0500

    Add JSDoc comments to updateDB.tsx

--- server-buddy/src/database/setters/updateDB.tsx
+++ server-buddy/src/database/setters/updateDB.tsx
@@ -5,10 +5,10 @@ import { collection, deleteDoc, doc, updateDoc } from "firebase/firestore"
 import { Item } from "../../objects/menuItem";
 
 export class updateDB {
-    /*
-    updateOrderStatus takes an order object and updates the orderStatus in the database
-    Order object is defined in objects/order.tsx
-    */
+    /**
+     * updateOrderStatus takes an order object and updates the orderStatus in the database
+     * @param order: Order object
+     */
     static async updateOrderStatus(order: Order) {
         const orderRef = collection(db, "Orders");
         const orderDoc = doc(orderRef, order.getOrderId());
@@ -17,10 +17,10 @@ export class updateDB {
         });
     }
 
-    /*
-    updateTicketStatus takes a ticket object and updates the ticketStatus in the database
-    Ticket object is defined in objects/ticket.tsx
-    */
+    /**
+     * updateTicketStatus takes a ticket object and updates the ticketStatus in the database
+     * @param ticket: Ticket object
+     */
     static async updateTicketStatus(ticket: Ticket) {
         const ticketRef = collection(db, "Tickets");
         const ticketDoc = doc(ticketRef, ticket.getTicketId());
@@ -32,7 +32,6 @@ export class updateDB {
     /**
      * deleteItem takes an item object and deletes it from the database
      * @param item: Item object
-     *  
      */
     static async deleteItem(item: Item) {
         const itemRef = collection(db, "MenuItems");
