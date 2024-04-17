414793e4534f792262a528c571262da9e9815a43
[carlsullivan] <[ces8059@utulsa.edu]>
2024-04-17 14:16:38 -0500

    Add deleteItem, voidOrder, and voidTicket methods to updateDB.tsx

--- server-buddy/src/database/setters/updateDB.tsx
+++ server-buddy/src/database/setters/updateDB.tsx
@@ -1,7 +1,8 @@
 import { db } from "../firebase"
 import { Order } from "../../objects/order"
 import { Ticket } from "../../objects/ticket"
-import { collection, doc, updateDoc } from "firebase/firestore"
+import { collection, deleteDoc, doc, updateDoc } from "firebase/firestore"
+import { Item } from "../../objects/menuItem";
 
 export class updateDB {
     /*
@@ -25,6 +26,40 @@ export class updateDB {
         const ticketDoc = doc(ticketRef, ticket.getTicketId());
         await updateDoc(ticketDoc, {
             ticketStatus: ticket.getTicketStatus()
+        });
+    }
+
+    /**
+     * deleteItem takes an item object and deletes it from the database
+     * @param item: Item object
+     *  
+     */
+    static async deleteItem(item: Item) {
+        const itemRef = collection(db, "MenuItems");
+        const itemDoc = doc(itemRef, item.getItemId());
+        await deleteDoc(itemDoc);
+    }
+    /**
+     * voidOrder takes an order object and updates the orderStatus to "void" in the database
+     * @param order: Order object
+     */
+    static async voidOrder(order: Order) {
+        const orderRef = collection(db, "Orders");
+        const orderDoc = doc(orderRef, order.getOrderId());
+        await updateDoc(orderDoc, {
+            orderStatus: "void"
+        });
+    }
+
+    /**
+     * voidTicket takes a ticket object and updates the ticketStatus to "void" in the database
+     * @param ticket: Ticket object
+     */
+    static async voidTicket(ticket: Ticket) {
+        const ticketRef = collection(db, "Tickets");
+        const ticketDoc = doc(ticketRef, ticket.getTicketId());
+        await updateDoc(ticketDoc, {
+            ticketStatus: "void"
         });
     }
 }
