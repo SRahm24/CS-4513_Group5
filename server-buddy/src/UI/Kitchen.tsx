267be3d61b25bd2820e54ec706920477f59689e7
Sajid Rahman <sar9727@utulsa.edu>
2024-04-27 14:46:37 -0500

    Working Kitchen - orders' statuses are now set to ready

--- server-buddy/src/UI/Kitchen.tsx
+++ server-buddy/src/UI/Kitchen.tsx
@@ -14,6 +14,7 @@ import { TicketManager } from './TicketManager';
 import { OrdersQueries } from '../database/queries/ordersQueries';
 import { Order } from '../objects/order';
 import { reload } from 'firebase/auth';
+import { Timestamp, serverTimestamp } from "firebase/firestore";
 import { updateDB } from '../database/setters/updateDB';
 
 const manager: TicketManager = new TicketManager();
@@ -30,124 +31,132 @@ const MenuProps = {
   },
 };
 
-  function createData(
-    ticketId: string,
-    employeeId: string,
-    tableId: number,
-    orderStatus: string,
-    menuItems: any[]
-  ) {
-    let orderId = "";
-    return { 
-      orderId,
-      ticketId,
-      tableId,
-      orderStatus,
-      menuItems
-    };
-  }
+function createData(
+  ticketId: string,
+  employeeId: string,
+  orderStatus: string,
+  tableId: number,
+  restaurantId: string,
+  orderDateTime: Timestamp,
+  menuItems: any[]
+) {
+  let orderId = "";
+  return {
+    orderId,
+    ticketId,
+    employeeId,
+    tableId,
+    restaurantId,
+    orderDateTime,
+    orderStatus,
+    menuItems
+  };
+}
 
-  function Row(props: { row: ReturnType<typeof createData> }) {
-    const {row} = props;
-    const [menuItem, setMenuItem]: any[] = React.useState(row.menuItems);
+function Row(props: { row: ReturnType<typeof createData> }) {
+  const { row } = props;
+  const [menuItem, setMenuItem]: any[] = React.useState(row.menuItems);
 
-    return (
-      <React.Fragment>
-        <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-              <Box sx={{ margin: 1 }}>
-                <Typography variant="h6" gutterBottom component="div">
-                  Order ID: {row.orderId}
-                </Typography>
-                <Table size="small" aria-label="purchases">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Table Id</TableCell>
-                      <TableCell>Item</TableCell>
-                      <TableCell align="right">Ticket Id</TableCell>
-                      <TableCell align="right">Status</TableCell>
-                      <TableCell align="right">Update</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {row.menuItems.map((menuItem: any) => (
-                      <TableRow key={row.orderId}>
-                        <TableCell component="th" scope="row">
-                          {row.tableId === -1 ? 'To-Go' : ''}
-                        </TableCell>
-                        <TableCell>{menuItem}</TableCell>
-                        <TableCell align="right">{row.ticketId}</TableCell>
-                        <TableCell align="right">{row.orderStatus}</TableCell>
-                        <TableCell align="right">
-                          <Button variant="contained" 
-                          onClick={async () => 
-                            {
-                              const bool = confirm('Are you sure you want to update this order?');
-                              if (bool && row.orderStatus == "In Progress") {
-                                console.log(row.orderId);
-                                const ticketOrder = await manager.getOrdersByTicket(row.ticketId);
-                                console.log(ticketOrder);
-                                console.log(ticketOrder[0]);
-                                ticketOrder[0].setOrderStatus("Ready");
-                                updateDB.updateOrderStatus(ticketOrder[0]);
-                              }
-                              else if (bool && row.orderStatus == "Ready") 
-                                {
-                                  //row.setOrderStatus("Completed");
-                                  updateDB.updateOrderStatus(menuItem);
-                                  alert('Order status updated to Completed');
-                                }
-                                else if (bool == false)
-                                {
-                                  alert('Order status not updated');
-                                }
-                            }}>
-                          UPDATE
-                          </Button>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </Box>
-          </TableCell>
-        </TableRow>
-      </React.Fragment>
+  const newOrder =
+    new Order(
+      row.orderId,
+      row.ticketId,
+      row.employeeId,
+      row.tableId,
+      row.restaurantId,
+      row.orderDateTime,
+      row.orderStatus,
+      menuItem
     );
-  }
 
-  function BasicTable() {
+  return (
+    <React.Fragment>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Box sx={{ margin: 1 }}>
+            <Typography variant="h6" gutterBottom component="div">
+              Order ID: {row.orderId}
+            </Typography>
+            <Table size="small" aria-label="purchases">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Table Id</TableCell>
+                  <TableCell>Item</TableCell>
+                  <TableCell align="right">Ticket Id</TableCell>
+                  <TableCell align="right">Status</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {row.menuItems.map((menuItem: any) => (
+                  <TableRow key={row.orderId}>
+                    <TableCell component="th" scope="row">
+                      {row.tableId === -1 ? 'To-Go' : ''}
+                    </TableCell>
+                    <TableCell>{menuItem}</TableCell>
+                    <TableCell align="right">{row.ticketId}</TableCell>
+                    <TableCell align="right">{row.orderStatus}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              <Button variant="text" color="primary"
+                onClick={async () => {
+                  const bool = confirm('Are you sure you want to update this order?');
+                  if (bool && row.orderStatus == "In Progress") {
+                    newOrder.setOrderStatus("Ready");
+                    updateDB.updateOrderStatus(newOrder);
+                  }
+                  else if (bool && row.orderStatus == "Ready") {
+                    newOrder.setOrderStatus("Completed");
+                    updateDB.updateOrderStatus(newOrder);
+                  }
+                  else if (bool == false) {
+                    alert('Order status not updated');
+                  }
+                }}>
+                UPDATE
+              </Button>
+            </Table>
+          </Box>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+function BasicTable() {
 
 
-    const [rows, setRows]: any[] = React.useState([]);
-    const [kitchenBool, setKitchenBool] = React.useState(false);
-    React.useEffect(() => {
-      setKitchenBool(true);
-      OrdersQueries.getKitchenOrders().then(orderData => setRows(orderData));
-    }, [kitchenBool]
+  const [rows, setRows]: any[] = React.useState([]);
+  const [kitchenBool, setKitchenBool] = React.useState(false);
+  React.useEffect(() => {
+    setKitchenBool(true);
+    OrdersQueries.getKitchenOrders().then(orderData => setRows(orderData));
+  }, [kitchenBool]
   )
 
-  
-    return (
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Table</TableCell>
-              <TableCell align="right">Items</TableCell>
-              <TableCell align="right">Status</TableCell>
-              <TableCell align="right">Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row: any) => (
-              <Row key={row.orderId} row={row} />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
-  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Kitchen Orders
+        </Typography>
+        </TableHead>
+        <TableBody>
+          {rows.map((row: any) => (
+            <Row key={row.orderId} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 function Kitchen() {
   return (
